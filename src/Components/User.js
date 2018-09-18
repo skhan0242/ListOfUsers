8ba915d7dcd977141d010c32436b7729db87362a
saleem khan <saleemkhan0242@gmail.com>
2018-09-18 09:17:37 +0530

    second commit

--- src/Components/User.js
+++ src/Components/User.js
@@ -3,13 +3,15 @@ import React, { Component } from 'react';
 class User extends Component {
   render() {
     return (
-      <div className="">
+      <div className="user-details-wrapper">
         <h2>Users Details</h2>
+          <div className="user-details">
             <p>First Name: {this.props.userDetails.first}</p>
             <p>Last Name: {this.props.userDetails.last}</p>
             <p>age: {this.props.userDetails.age}</p>
             <p>Location: {this.props.userDetails.location}</p>
             <p>Description: {this.props.userDetails.description}</p>
+          </div>
       </div>
     );
   }
